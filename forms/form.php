31a7fa34bcbcc055e2945fc3b9440114b464bdcf
Leonardo Tiago <pequenotiago@gmail.com>
2025-03-07 05:06:18 +0100

    Validando os campos do formulario

--- forms/form.php
+++ forms/form.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 echo "Nome: {$_POST['nome']}";
 echo '<hr>';
 echo "Senha: {$_POST['senha']}";
@@ -13,4 +14,40 @@ echo '<hr>';
 echo "Estado: {$_POST['estado']}";
 echo '<hr>';
 echo "descricao: {$_POST['descricao']}";
+echo '<hr>';*/
+
+if($_REQUEST['nome'] != '' && strlen($_REQUEST['nome']) > 3) {
+    echo "Nome: {$_REQUEST['nome']}";
+} else {
+    echo 'O campo nome nao foi validado!';
+}
+echo '<hr>';
+if($_REQUEST['senha'] != '' && strlen($_REQUEST['senha']) > 4) {
+    echo "Senha: {$_REQUEST['senha']}";
+} else {
+    echo 'O campo senha nao foi validado!';
+}
+echo '<hr>';
+if($_REQUEST['email'] != '') {
+    echo "E-mail: {$_REQUEST['email']}";
+} else {
+    echo 'O campo senha nao foi validado!';
+}
+
+echo '<hr>';
+echo "Concordou? " . isset($_REQUEST['concordo']) && $_REQUEST['concordo'] != '' ? 'Sim' : 'Nao';
+echo '<hr>';
+if($_REQUEST['sexo'] != '') {
+    echo "Sexo: {$_REQUEST['sexo']}";
+} else {
+    echo 'O campo sexo nao foi validado!';
+}
+echo '<hr>';
+if($_REQUEST['estado'] != '') {
+    echo "Estado: {$_REQUEST['estado']}";
+} else {
+    echo 'O campo sexo nao foi validado!';
+}
+echo '<hr>';
+echo "descricao: {$_REQUEST['descricao']}";
 echo '<hr>';
