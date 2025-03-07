220c74b6b9ede074f09e543c86530f47d4d125e7
Leonardo Tiago <pequenotiago@gmail.com>
2025-03-07 06:52:32 +0100

    uploads de múltiplos arquivos

--- uploads/upload.php
+++ uploads/upload.php
@@ -1,12 +1,26 @@
 <?php
-
+/*
 $photo = $_FILES['imagem'];
 $path = 'imgs/';
 
-
+/
 
 if(move_uploaded_file($photo['tmp_name'], $path . $photo['name'])) {
     echo 'Sucesso!';
 } else {
     echo 'Falhar no upload';
 }
+    */
+    $photo = $_FILES['imagem'];
+    $path = 'imgs/';
+    
+  for($i = 0; $i < count($photo['name']); $i ++) {
+    $j = $i + 1;
+      
+    if(move_uploaded_file($photo['tmp_name'][$i], $path . $photo['name'][$i])) {
+        echo "Sucesso ao mover o arquivo {$j}! <br>";
+    } else {
+        echo "Falhar no upload {$j} <br>";
+    }
+  }
+    
