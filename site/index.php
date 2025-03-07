129de62f5ef35dd86ec072706f86e0c2a8b881e4
Leonardo Tiago <pequenotiago@gmail.com>
2025-03-07 02:52:57 +0100

    Exemplo de site em php

---
+++ site/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu site em PHP</title>
+</head>
+<body>
+    <?php
+        //include('include/header.php'); 
+    ?>
+  
+    <div>
+        <?php
+            echo 'Bem-vindo ao nosso site';
+        ?>
+    </div>
+    <?php
+        include_once('include/footer.php'); 
+    ?>
+</body>
+</html>
